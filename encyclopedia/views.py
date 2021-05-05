52e3b1a0dfbc819901ed49a40c8298505a59b858
GretaRob <greta.robichaud@gmail.com>
2021-05-05 19:55:15 +0700

    enabled searching for entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . import util
 import markdown2
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -14,3 +16,20 @@ def entry(request, title):
     return render(request, 'encyclopedia/entry.html', {
         'content': markdown2.markdown(util.get_entry(title)), 'title': title
     })
+
+
+def search(request):
+    search = request.GET.get('q', '')
+    entries = util.list_entries()
+    if search:
+        # if search query matches one of the entries
+        return HttpResponseRedirect(reverse('entry', kwargs={'title': search}))
+    else:
+        results = []
+        for entry in entries:
+            if search in entry:
+                results.append(entry)
+
+        return render(request, 'encyclopedia/search.html', {
+            'results': results
+        })
